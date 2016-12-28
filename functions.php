4b44e6010d2a0f4f23afb2f6c445439976e05ba9
Michal Skora <m.skora@futuremind.com>
2016-12-28 16:53:43 +0100

    Edition custom type

--- functions.php
+++ functions.php
@@ -16,7 +16,8 @@ $sage_includes = [
   'lib/titles.php',    // Page titles
   'lib/wrapper.php',   // Theme wrapper class
   'lib/customizer.php', // Theme customizer
-  'lib/cleaning.php'   // Cleanings
+  'lib/cleaning.php',   // Cleanings
+  'lib/custom-types.php'   // Custom types
 ];
 
 foreach ($sage_includes as $file) {
---
+++ lib/custom-types.php
@@ -0,0 +1,40 @@
+<?php
+    add_action( 'init', 'edition_post_type' );
+    function edition_post_type() {
+        /**
+         * Register a achievement post type.
+         */
+        $labels_achivments = array(
+            'name'               => 'Edycje',
+            'singular_name'      => 'Edycja',
+            'menu_name'          => 'Edycje',
+            'name_admin_bar'     => 'Edycje',
+            'add_new'            => 'Nowa edycja',
+            'add_new_item'       => 'Dodaj nową edycje',
+            'new_item'           => 'Nowa edycja',
+            'edit_item'          => 'Edytuj edycje',
+            'view_item'          => 'Zobacz edycje',
+            'all_items'          => 'Wszystkie edycje',
+            'search_items'       => 'Wyszukaj edycje',
+            'parent_item_colon'  => 'Nadrzędne edycje:',
+            'not_found'          => 'Nie znaleziono edycji',
+            'not_found_in_trash' => 'Nie znaleziono edycji w koszu'
+        );
+        $args = array(
+            'labels'             => $labels_achivments,
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'query_var'          => true,
+            'rewrite'            => array( 'slug' => 'edition' ),
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => null,
+            'supports'           => array('title', 'thumbnail', 'excerpt'),
+            'menu_icon'          => 'dashicons-media-video'
+        );
+        register_post_type( 'ak_achivments', $args );
+    }
+?>
