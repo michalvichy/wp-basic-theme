9aa3a4ef78f570a27a83704e2f295bbb33494de5
Michal Skora <m.skora@futuremind.com>
2017-01-17 14:46:38 +0100

    Smaller footer

--- single-edition.php
+++ single-edition.php
@@ -9,6 +9,11 @@
     $date = date_create($date_field);
     $date = strftime('%d %B %G', strtotime($date_field));
     $city = $fields['edition_city'];
+
+    $has_timeline = empty($fields['edition_timeline']) ? false : true;
+    $has_menu = empty($fields['edition_menu']) ? false : true;
+    $has_chefs = empty($fields['edition_chefs']) ? false : true;
+    $has_gallery = empty($fields['edition_gallery']) ? false : true;
 ?>
 
 
@@ -24,18 +29,30 @@
                     <div class="c-dropdown js-dropdown">
                         <span>Relacja</span>
                         <ul class="c-dropdown__list u--inline-list">
-                            <li><span data-target="timeline" class="js-overlay-open">Program</span></li>
-                            <li><span data-target="menu" class="js-overlay-open">Menu</span></li>
-                            <li><span data-target="chefs" class="js-overlay-open">Szefowie kuchni</span></li>
+                            <?php if ($has_timeline): ?>
+                                <li><span data-target="timeline" class="js-overlay-open">Program</span></li>
+                            <?php endif; ?>
+                            <?php if ($has_menu): ?>
+                                <li><span data-target="menu" class="js-overlay-open">Menu</span></li>
+                            <?php endif; ?>
+                            <?php if ($has_chefs): ?>
+                                <li><span data-target="chefs" class="js-overlay-open">Szefowie kuchni</span></li>
+                            <?php endif; ?>
                         </ul>
                     </div>
                 </div>
                 <div class="col-xs-5 col-sm-8 col-lg-9 phone--hide desktop--show">
                     <ul class="c-edition__top-menu u--inline-list">
-                        <li><button data-target="timeline" class="c-btn c-btn--primary js-overlay-open">Program</button></li>
-                        <li><button data-target="menu" class="c-btn c-btn--primary js-overlay-open">Menu</button></li>
+                        <?php if ($has_timeline): ?>
+                            <li><button data-target="timeline" class="c-btn c-btn--primary js-overlay-open">Program</span></li>
+                        <?php endif; ?>
+                        <?php if ($has_menu): ?>
+                            <li><button data-target="menu" class="c-btn c-btn--primary js-overlay-open">Menu</span></li>
+                        <?php endif; ?>
                         <li><button class="c-btn c-btn--primary active">Relacja</button></li>
-                        <li><button data-target="chefs" class="c-btn c-btn--primary js-overlay-open">Szefowie kuchni</button></li>
+                        <?php if ($has_chefs): ?>
+                            <li><button data-target="chefs" class="c-btn c-btn--primary js-overlay-open">Szefowie kuchni</span></li>
+                        <?php endif; ?>
                     </ul>
                 </div>
             </div>
@@ -46,12 +63,14 @@
 <div class="o-base u--border-top u--bg-black">
     <div class="o-container">
         <div class="o-section">
-            <div class="u--align-center">
-                <div class="c-edition__gallery-intro u--align-center">
-                    <?php the_post_thumbnail('edition-thumb', array( 'class' => 'c-image-hover' )); ?>
-                    <button class="c-btn c-btn--primary js-overlay-open" data-target="gallery">Zobacz fotorelację</a>
+            <?php if ($has_gallery): ?>
+                <div class="u--align-center">
+                    <div class="c-edition__gallery-intro u--align-center">
+                        <?php the_post_thumbnail('edition-thumb', array( 'class' => 'c-image-hover' )); ?>
+                        <button class="c-btn c-btn--primary js-overlay-open" data-target="gallery">Zobacz fotorelację</a>
+                    </div>
                 </div>
-            </div>
+            <?php endif; ?>
 
             <h2 class="c-headline c-headline--center">Relacja</h2>
 
--- templates/footer.php
+++ templates/footer.php
@@ -1,32 +1,38 @@
+<?php
+    $args = array(
+        'posts_per_page' => 1,
+    	'offset' => 0,
+    	'orderby' => 'date',
+    	'order' => 'DESC',
+        'post_type' => 'edition',
+        'post_status' => 'publish',
+        'suppress_filters' => true
+    );
+
+    $last_edition = wp_get_recent_posts( $args, ARRAY_A );
+    $last_edition_ID = $last_edition[0]['ID'];
+?>
+
 <div class="o-base">
     <footer class="c-footer">
         <div class="o-container">
-            <div class="row middle-xs center-xs start-lg">
+            <div class="row middle-xs center-xs start-md">
                 <div class="col-xs-12 col-md-5 last-xs first-md">
-                    <img src="<?= get_template_directory_uri(); ?>/dist/images/footer_logo.png" alt="footer-logo">
-                    <p class="u--text-12 u--text-gray">Copyright © 2016 Nespresso. All Rights Reserved.</p>
+                    <img src="<?= get_template_directory_uri(); ?>/dist/images/footer_logo.png" alt="footer-logo" class="c-footer__logo">
+                    <p class="c-footer__copy u--text-12 u--text-gray">Copyright © 2016 Nespresso. All Rights Reserved.</p>
                 </div>
-                <div class="col-xs-12 col-md-2">
-                    <a href="/" class="u--uppercase">Strona główna</a>
-                </div>
-                <div class="col-xs-12 col-md-4">
+                <div class="col-md-7 desktop--right">
                     <ul class="c-footer__menu u--inline-list">
                         <li class="c-footer__menu-item">
-                            <a href="#" class="c-footer__menu-link">Edycja IV</a>
+                            <a href="/" class="c-footer__menu-link">Strona główna</a>
                         </li>
                         <li class="c-footer__menu-item">
-                            <a href="#" class="c-footer__menu-link">Edycja III</a>
+                            <a href="<?= get_permalink($last_edition_ID); ?>" class="c-footer__menu-link"><?= get_the_title($last_edition_ID); ?></a>
                         </li>
                         <li class="c-footer__menu-item">
-                            <a href="#" class="c-footer__menu-link">Edycja II</a>
-                        </li>
-                        <li class="c-footer__menu-item">
-                            <a href="#" class="c-footer__menu-link">Edycja I</a>
+                            <span class="c-footer__menu-link js-overlay-open" data-target="contact">Kontakt</span>
                         </li>
                     </ul>
-                </div>
-                <div class="col-xs-12 col-md-1">
-                    <span class="u--uppercase js-overlay-open" data-target="contact">Kontakt</span>
                 </div>
             </div>
         </div>
