7ad851b342d3d0467007eb9eb0c4e28b8007f176
Michal Skora <m.skora@futuremind.com>
2017-01-18 14:24:33 +0100

    Editions in footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,18 +1,3 @@
-<?php
-    $args = array(
-        'posts_per_page' => 1,
-    	'offset' => 0,
-    	'orderby' => 'date',
-    	'order' => 'DESC',
-        'post_type' => 'edition',
-        'post_status' => 'publish',
-        'suppress_filters' => true
-    );
-
-    $last_edition = wp_get_recent_posts( $args, ARRAY_A );
-    $last_edition_ID = $last_edition[0]['ID'];
-?>
-
 <div class="o-base">
     <footer class="c-footer">
         <div class="o-container">
@@ -27,7 +12,7 @@
                             <a href="/" class="c-footer__menu-link">Strona główna</a>
                         </li>
                         <li class="c-footer__menu-item">
-                            <a href="<?= get_permalink($last_edition_ID); ?>" class="c-footer__menu-link"><?= get_the_title($last_edition_ID); ?></a>
+                            <span class="c-footer__menu-link js-overlay-open" data-target="editions">Edycje</span>
                         </li>
                         <li class="c-footer__menu-item">
                             <span class="c-footer__menu-link js-overlay-open" data-target="contact">Kontakt</span>
